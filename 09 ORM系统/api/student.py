39d2d564c03cf5c07f63f07135a9f95a1926327c
EiserLiu <1071519731@qq.com>
2023-12-20 18:05:12 +0800

    12月17日fastAPI学习内容

--- 09 ORM系统/api/student.py
+++ 09 ORM系统/api/student.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from fastapi import APIRouter, Request
-from models import *
-from pydantic import BaseModel
-from typing import List, Union
+from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
+from models import *
+from pydantic import BaseModel, validator
 
 student_api = APIRouter()
 
@@ -35,13 +37,21 @@ async def getAllStudent():
     # values 查询
     # stus = await Student.filter(sno__range=[1,30])
     # stus = await Student.all().values("name")
-    stus = await Student.all().values("name", "sno")
+    # stus = await Student.all().values("name", "sno")
     # print(stus)
 
-    return {
+    # 一对多查询 多对多查询
+    stu = await Student.get(name="刘泽普")
 
-        "操作": stus
-    }
+    print(stu.name)
+    print(stu.sno)
+    print(stu.clas_id)
+    print(await stu.clas.values("name"))
+    student = await Student.all().values("name", "clas__name")
+    print(await stu.course.all().values("name", "teacher__name"))
+    student = await Student.all().values("name", "clas__name", "course__name")
+
+    return student
 
 
 @student_api.get("/index.html")
@@ -57,29 +67,67 @@ async def getAllStudent(request: Request):
     )
 
 
+class StudentIn(BaseModel):
+    name: str
+    pwd: str
+    sno: int
+    clas_id: int
+    courses: List[int] = []
+
+    @validator("name")
+    def name_mast_alpha(cls, value):
+        assert value.isalpha(), "name must be alpha"
+        return value
+
+    @validator("sno")
+    def sno_validate(cls, value):
+        assert 0 < value < 100, "学号要在0到100之间"
+        return value
+
+
 @student_api.post("/")
-def addStudent():
-    return {
-        "操作": "添加学生"
-    }
+async def addStudent(student_in: StudentIn):
+    # 插入到数据库
+    # 方式1
+    # student = Student(name=student_in.name, pwd=student_in.pwd, sno=student_in.sno, _class_id=student_in.clas_id)
+    # await student.save()  # 插入到数据库student表
+
+    # 方式2
+    student = await Student.create(name=student_in.name, pwd=student_in.pwd, sno=student_in.sno,
+                                   clas_id=student_in.clas_id)
+
+    # 多对多的关系绑定
+    choose_courses = await Course.filter(id__in=student_in.courses)
+    await student.course.add(*choose_courses)
+
+    return student
 
 
 @student_api.get("/{student_id}")
-def getStudent(student_id: int):
-    return {
-        "操作": f"查看id={student_id}的学生"
-    }
+async def getStudent(student_id: int):
+    student = await Student.get(id=student_id)
+    return student
 
 
 @student_api.put("/{student_id}")
-def upStudent(student_id: int):
-    return {
-        "操作": f"更新id={student_id}的学生"
-    }
+async def upStudent(student_id: int, student_in: StudentIn):
+    data = student_in.dict()
+    courses = data.pop("courses")
+    print(data)
+    await Student.filter(id=student_id).update(**data)
+    # 设置多对多的选修课
+    edit_stu = await Student.get(id=student_id)
+    choose_courses = await Course.filter(id__in=courses)
+    await edit_stu.course.clear()
+    await edit_stu.course.add(*choose_courses)
+
+    return await Student.get(id=student_id)
 
 
 @student_api.delete("/{student_id}")
-def upStudent(student_id: int):
-    return {
-        "操作": f"删除id={student_id}的学生"
-    }
+async def upStudent(student_id: int):
+    deleteCoune = await Student.filter(id=student_id).delete()
+    if not deleteCoune:
+        raise HTTPException(status_code=404, detail=f"主键为{student_id}的学生不存在")
+
+    return {}
